d9e3c1c433a71e112085ddeecbac96cb2e102803
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2020-11-20 01:39:08 +0300

    lesson 5.6 Constant

---
+++ 5.6_Constant/Constant.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const pi = 3.14
+
+const (
+	One = iota
+	Two
+	Three
+)
+
+func  main()  {
+	const x string = "Hello World"
+	println(x)
+
+	fmt.Println("Математическая константа PI =", pi)
+
+	// pi = 30
+
+	// Выведет 0 1 2, потмоу что iota начинается с 0
+	fmt.Println(One, Two, Three)
+
+}
