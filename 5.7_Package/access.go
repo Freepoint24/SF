4ee5b64363a1de98d6dfc98d7687fc80c8df5f30
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2020-11-21 00:01:49 +0300

    lesson 5.7 Packege

---
+++ 5.7_Package/access.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"module1/test"
+)
+
+func  main()  {
+	// Никаких проблем с вызовом функции
+	test.Public()
+
+	// При запуск программы выдаст ошибку
+	test.private()
+
+
+}
