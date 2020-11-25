6d85019a5e949f882ff5933170cf2ae26e706575
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2020-11-25 19:14:42 +0300

    homework

---
+++ 6.8_calculator/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number1, number2, output float32
+	var operator string
+
+	//ввод числа 1
+	//	fmt.Print("Введите число1:")
+	fmt.Scan(&number1)
+
+	//ввод оператора
+	//fmt.Print("оператор:")
+	fmt.Scan(&operator)
+
+	//ввод числа 1
+	//fmt.Print("Введите число2:")
+	fmt.Scan(&number2)
+
+	switch operator {
+	case "*":
+		output = number1 * number2
+		fmt.Println("Результат умножения:", output)
+	case "/":
+				if number2 == 0 {
+					fmt.Println("ошибка")
+				}else {
+					output = number2 / number1
+					fmt.Println("Результат деления:", output)
+				}
+	case "+":
+		output = number1 + number2
+		fmt.Println("Результат сложения:", output)
+	case "-":
+		output = number1 - number2
+		fmt.Println("Результат вычитания:", output)
+	default:
+		fmt.Println("Ошибка")
+	}
+
+}
