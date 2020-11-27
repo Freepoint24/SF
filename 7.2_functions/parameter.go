ed602526fb2923e443914113e95db070efb2ba83
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2020-11-27 23:42:37 +0300

    lesson 7.2 parameter

---
+++ 7.2_functions/parameter.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	someVariable := "some text"
+
+	ourCallback := func() { fmt.Println(someVariable) }
+
+	runCallback(ourCallback)
+
+}
+
+func runCallback(callback func()) {
+
+}
