db41861f35e5833906625447168b17a6c2eefa8e
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2020-12-07 22:55:07 +0300

    lesson 7.5 struct

--- 7.5_struct/simple2/calc/calc.go
+++ 7.5_struct/simple2/calc/calc.go
@@ -1,15 +1,27 @@
 package calc
 
-import "fmt"
+import (
+	"log"
+)
 
-type calculator struct {
-	x float64
-	y float64
-	r float64
+type calculator struct {}
+
+func NewCalculator() calculator  {
+	return calculator{}
 }
 
-func NewCalc() {
-	c := calculator{5, 7, 8}
+func (c *calculator) Calculate(number1, number2 float64, operator string) float64 {
+	switch operator {
+	case "+":
+			return c.add(number1, number2)
+	default:
+		log.Printf("ошибка")
+		return 0
+	}
 
-	fmt.Println(c)
+//	return 0
+}
+
+func (c *calculator) add(number1, number2 float64) float64 {
+	return number1 + number2
 }
