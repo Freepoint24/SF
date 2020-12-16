c859a8d4362a924321bf9f9029d90d40c7f0dae7
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2020-12-16 11:08:25 +0300

    lesson 8.3 error

--- 8.5_interface/home.go
+++ 8.5_interface/home.go
@@ -1,1 +1,39 @@
-package main
+package electronic
+
+import "fmt"
+
+type Phone interface {
+	Brand() string
+	Model() string
+	Type() string
+}
+
+type StationPhone interface {
+	ButtonsCount() int
+}
+
+type Smartphone interface {
+	OS() string // название ос
+}
+
+type applePhone struct {
+	Brand string
+	Model string
+	OS string
+}
+type androidPhone struct {
+	Brand string
+	Model string
+	OS string
+}
+type radioPhone struct {
+	Brand string
+	Model string
+	OS string
+}
+
+func (x *Smartphone) Type() string  {
+	fmt.Println(Type)
+	applePhone
+	androidPhone
+}
