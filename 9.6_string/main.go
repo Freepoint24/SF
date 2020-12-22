b12d4e5d6efcc472114ca4cf1a9b1073140ad732
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2020-12-22 19:38:52 +0300

    lesson 9.6 string

---
+++ 9.6_string/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("word"), len("слово"))
+	//для хранения слова word используется 4 бита
+	//для хранения слова слово используется 10 бит
+}
