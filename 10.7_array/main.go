4850ec26b646ac754591db70fd7148b1f3e265c6
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2021-01-22 20:00:42 +0300

    lesson 10

---
+++ 10.7_array/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+"fmt"
+"math"
+)
+
+func main() {
+
+	arr := []int{34, 67, 12, 3, 4, 9, 108, 6, 4, 3, 1, 100, 39}
+	maxVal := findMax(arr)
+	fmt.Printf("Max value in array %v -> %v \n", arr, maxVal)
+
+	// Max value in array [34 67 12 3 4 9 108 6 4 3 1 100 39] -> 108
+}
+
+func findMax(array []int) (max int) {
+	max = math.MinInt64
+	for _, val := range array {
+		if val > max {
+			max = val
+		}
+	}
+
+	return max
+}
