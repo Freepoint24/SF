3698b4c1e323b636a9d07a6fbb91d0c07d23b5f7
Freepoint24 <5443006@gmail.com>
2021-01-29 22:07:21 +0300

    lesson 12.5_Insertion sort homework end

--- Algoritm/12.5_Insertion sort/12.5.1_hw.go
+++ Algoritm/12.5_Insertion sort/12.5.1_hw.go
@@ -9,17 +9,20 @@ import (
 //Insertion sort (Сортировка выбором)
 //Задание 12.5.1
 //Реализуйте сортировку вставками.
-//https://play.golang.org/p/OHWF2q482al
 func init() {
 	rand.Seed(time.Now().UnixNano()) // необходимо для того, чтобы рандом был похож на рандомный
 }
 
 func main() {
+	//ar :=[]int{6,5, 3, 1, 8, 7, 2, 4} // пример из задания
+	//ar :=[]int{1, 5, 6, 0, 10, -7, 3, 8, 4, 2, 7}
 	ar := make([]int, 50)
 	for i := range ar {
 		ar[i] = rand.Intn(200) - 100 // ограничиваем случайно значение от [-100;100]
 	}
 
+	fmt.Printf("Unsorted list:\t%v\n", ar) //не сортированный массив
+	fmt.Println("")
 	insertionSort(ar)
 	fmt.Println("")
 	fmt.Printf("Sorted list:\t%v\n", ar) //отсортированный масссив
@@ -27,12 +30,11 @@ func main() {
 
 //Реализуйте двунаправленную сортировку выбором.
 func insertionSort(ar []int) {
-	fmt.Printf("Unsorted list:\t%v\n", ar) //не сортированный массив
-	fmt.Println("")
+
 	for i := 1; i < len(ar); i++ {
 		for j := i; j != 0 && ar[j] < ar[j-1]; j-- {
 			ar[j-1], ar[j] = ar[j], ar[j-1]
 		}
-		//fmt.Printf("Sorting ...:\t%v\n", ar)// визуализация
+		fmt.Printf("Sorting ...:\t%v\n", ar) // визуализация
 	}
 }
---
+++ Algoritm/12.5_Insertion sort/readme.txt
@@ -0,0 +1,5 @@
+//Insertion sort (Сортировка выбором)
+//Задание 12.5.1
+//Реализуйте сортировку вставками.
+https://play.golang.org/p/f-d8UycUN1R
+https://play.golang.org/p/gYOUiTB3_U-
