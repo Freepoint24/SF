becbef802e6afd2a3bf2bfd1b798703d41fe849f
Freepoint24 <5443006@gmail.com>
2021-01-30 14:41:24 +0300

    lesson 12.7_Fast_sorting

--- Algoritm/12.7_Fast_sorting/12.7.1_hw.go
+++ Algoritm/12.7_Fast_sorting/12.7.1_hw.go
@@ -6,59 +6,49 @@ import (
 	"time"
 )
 
-//Основная идея - берем опорную точку (pivot), проходим массив,
-//чтобы элементы, которые меньше опорной точки оказались слева от нее, а которые больше - справа.
-//Дальше берем часть массива до опорной точки и вторую часть после опортной точки, повторяем на них сортировку.
-//Продолжаем до того момента, как сортируемая часть массива будет пустой или состоять из одного элемента.
-//https://play.golang.org/p/cHcbiE0Ugry
+//Задание 12.7.1
+//Реализуйте быструю сортировку.
+//https://play.golang.org/p/ltbNwisNTFg
+
+var index, start, end int
+
 func init() {
 	rand.Seed(time.Now().UnixNano()) // необходимо для того, чтобы рандом был похож на рандомный
 }
 
 func main() {
-	//ar := make([]int, 50)
-	//for i := range ar {
-	//	ar[i] = rand.Intn(200) - 100 // ограничиваем случайно значение от [-100;100]
-	ar := []int{9, 14, 5, -6, 0, 2, -8, 11}
+	//ar :=[]int{6,5, 3, 1, 8, 7, 2, 4} // пример из задания
+	//ar :=[]int{1, 5, 6, 0, 10, -7, 3, 8, 4, 2, 7}
+	ar := make([]int, 50)
+	for i := range ar {
+		ar[i] = rand.Intn(200) - 100 // ограничиваем случайно значение от [-100;100]
+	}
 	fmt.Printf("Unsorted list:\t%v\n", ar)
-	quickSort(ar)
+	//fmt.Println("")
+	quickSort(ar, 0, len(ar)-1)
+	//fmt.Println("")
 	fmt.Printf("Sorted list:\t%v\n", ar)
-
 }
 
-//quickSort(ar)
-//fmt.Println(ar)
-//}
-func quickSort(ar []int) {
-	if len(ar) <= 1 {
+// ваш код здесь
+func quickSort(ar []int, start, end int) {
+	if start >= end {
 		return
 	}
 
-	split := partition(ar)
+	pivot := ar[start]
+	i := start + 1
 
-	quickSort(ar[:split])
-	quickSort(ar[split:])
-
-}
-
-func partition(ar []int) int {
-	pivot := ar[len(ar)/2]
-	left := 0
-	right := len(ar) - 1
-	for {
-		for ; ar[left] < pivot; left++ {
+	for j := start; j <= end; j++ {
+		if pivot > ar[j] {
+			ar[i], ar[j] = ar[j], ar[i]
+			i++
 		}
-		for ; ar[right] > pivot; right-- {
-		}
-		if left >= right {
-			return right
-		}
-		swap(ar, left, right)
+		fmt.Printf("Sorting ...:\t%v\n", ar) // визуализация
 	}
-}
 
-func swap(ar []int, i, j int) {
-	tmp := ar[i]
-	ar[i] = ar[j]
-	ar[j] = tmp
+	ar[start], ar[i-1] = ar[i-1], ar[start]
+
+	quickSort(ar, start, i-2)
+	quickSort(ar, i, end)
 }
