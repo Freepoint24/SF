d61f4aedd49f698407dab42190981f7969f1290b
Freepoint24 <5443006@gmail.com>
2021-01-30 14:42:03 +0300

    lesson 12.8_bench

--- Algoritm/12.8_bench/sort3/sort_hw.go
+++ Algoritm/12.8_bench/sort3/sort_hw.go
@@ -61,3 +61,72 @@ func insertionSort(ar []int) {
 		fmt.Printf("Sorting ...:\t%v\n", ar) // визуализация
 	}
 }
+
+//ЗАДАНИЕ 12.6.1
+//Реализуйте сортировку слиянием, Merge sort
+//https://play.golang.org/p/UiNZ2qSRdhd
+// ваш код здесь
+func mergeSort(m []int) []int {
+	if len(m) <= 1 {
+		return m
+	}
+
+	mid := len(m) / 2
+	left := m[:mid]
+	right := m[mid:]
+
+	left = mergeSort(left)
+	right = mergeSort(right)
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	var result []int
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) > 0 && len(right) > 0 {
+			if left[0] <= right[0] {
+				result = append(result, left[0])
+				left = left[1:]
+			} else {
+				result = append(result, right[0])
+				right = right[1:]
+			}
+		} else if len(left) > 0 {
+			result = append(result, left[0])
+			left = left[1:]
+		} else if len(right) > 0 {
+			result = append(result, right[0])
+			right = right[1:]
+		}
+	}
+	return result
+}
+
+//
+//Задание 12.7.1
+//Реализуйте быструю сортировку.
+//https://play.golang.org/p/ltbNwisNTFg
+// ваш код здесь
+func quickSort(ar []int, start, end int) {
+	if start >= end {
+		return
+	}
+
+	pivot := ar[start]
+	i := start + 1
+
+	for j := start; j <= end; j++ {
+		if pivot > ar[j] {
+			ar[i], ar[j] = ar[j], ar[i]
+			i++
+		}
+	}
+
+	ar[start], ar[i-1] = ar[i-1], ar[start]
+
+	quickSort(ar, start, i-2)
+	quickSort(ar, i, end)
+}
+
+//
