9f92357041e0e05db4b6b116baf813fa46b981b5
Freepoint24 <61135009+Freepoint24@users.noreply.github.com>
2021-02-01 20:20:05 +0300

    lesson 12.8_bench hw

--- Algoritm/12.8_bench/sort3/sort_test.go
+++ Algoritm/12.8_bench/sort3/sort_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 )
@@ -11,6 +10,7 @@ import (
 //12.3.2
 //алгоритм сортировки пузырьком - bubble sort, отсортирует массив int по возрастанию.
 func BenchmarkBubbleSort(b *testing.B) {
+	b.ReportAllocs()
 	b.Run("small arrays", func(b *testing.B) {
 		b.StopTimer()
 		for i := 0; i < b.N; i++ {
@@ -40,11 +40,36 @@ func BenchmarkBubbleSort(b *testing.B) {
 			b.StopTimer()
 		}
 	})
+	//Добавить проверки для других размеров входных данных, отличающихся от примера, попробовать использовать
+	//большие и меньшие максимальные числа.
+	//малый размер данных
+	//	b.Run("SMOLL_small arrays", func(b *testing.B) {
+	//		b.StopTimer()
+	//		for i := 0; i < b.N; i++ {
+	//			ar := generateSlice(10, 100)
+	//			b.StartTimer()
+	//			bubbleSort(ar)
+	//			b.StopTimer()
+	//		}
+	//	})
+	//большой размер данныйх
+	//правал теста
+	//	b.Run("BIG_big arrays", func(b *testing.B) {
+	//		b.StopTimer()
+	//		for i := 0; i < b.N; i++ {
+	//			ar := generateSlice(100000, 400000)
+	//			b.StartTimer()
+	//			bubbleSort(ar)
+	//			b.StopTimer()
+	//		}
+	//	})
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
 //Задание 12.4.1
 //Реализуйте сортировку выбором Selection sort, работающую «слева направо»
 func BenchmarkSelectionSort(b *testing.B) {
+	b.ReportAllocs()
 	b.Run("small arrays", func(b *testing.B) {
 		b.StopTimer()
 		for i := 0; i < b.N; i++ {
@@ -74,28 +99,47 @@ func BenchmarkSelectionSort(b *testing.B) {
 			b.StopTimer()
 		}
 	})
+	//Добавить проверки для других размеров входных данных, отличающихся от примера, попробовать использовать
+	//большие и меньшие максимальные числа.
+	//малый размер данных
+	//b.Run("SMOLL_small arrays", func(b *testing.B) {
+	//	b.StopTimer()
+	//	for i := 0; i < b.N; i++ {
+	//		ar := generateSlice(5, 10)
+	//		b.StartTimer()
+	//		bubbleSort(ar)
+	//		b.StopTimer()
+	//	}
+	//})
+	//большой размер данныйх
+	//провал теста
+	//b.Run("BIG_big arrays", func(b *testing.B) {
+	//	b.StopTimer()
+	//	for i := 0; i < b.N; i++ {
+	//		ar := generateSlice(100000, 400000)
+	//		b.StartTimer()
+	//		bubbleSort(ar)
+	//		b.StopTimer()
+	//	}
+	//})
 }
 
-//Задание 12.4.3
-//Selection sort (Сортировка выбором)
-//func BenchmarkSelectionSort(b *testing.B) {
-//	b.SetBytes(10)
-//}
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Задание 12.5.1
 //Insertion sort (Сортировка выбором)
 func BenchmarkInsertionSort(b *testing.B) {
+	b.ReportAllocs()
 	// провал теста
-	fmt.Println("BenchmarkInsertionSort/ - small arrays - Test error")
-	//b.Run("small arrays", func(b *testing.B) {
-	//	b.StopTimer()
-	//	for i := 0; i < b.N; i++ {
-	//		ar := generateSlice(10, 10)
-	//		b.StartTimer()
-	//		bubbleSort(ar)
-	//		b.StopTimer()
-	//	}
-	//})
+	//fmt.Println("BenchmarkInsertionSort/ - small arrays - Test error")
+	b.Run("small arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10, 10)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
 
 	b.Run("middle arrays", func(b *testing.B) {
 		b.StopTimer()
@@ -116,32 +160,57 @@ func BenchmarkInsertionSort(b *testing.B) {
 			b.StopTimer()
 		}
 	})
+	//Добавить проверки для других размеров входных данных, отличающихся от примера, попробовать использовать
+	//большие и меньшие максимальные числа.
+	//малый размер данных
+	//b.Run("SMOLL_small arrays", func(b *testing.B) {
+	//	b.StopTimer()
+	//	for i := 0; i < b.N; i++ {
+	//		ar := generateSlice(5, 10)
+	//		b.StartTimer()
+	//		bubbleSort(ar)
+	//		b.StopTimer()
+	//	}
+	//})
+	//большой размер данныйх
+	//провал теста
+	//b.Run("BIG_big arrays", func(b *testing.B) {
+	//	b.StopTimer()
+	//	for i := 0; i < b.N; i++ {
+	//		ar := generateSlice(100000, 400000)
+	//		b.StartTimer()
+	//		bubbleSort(ar)
+	//		b.StopTimer()
+	//	}
+	//})
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 //ЗАДАНИЕ 12.6.1
 //Реализуйте сортировку слиянием, Merge sort
 func BenchmarkMergeSort(b *testing.B) {
-	//b.Run("small arrays", func(b *testing.B) {
-	fmt.Println("BenchmarkMergeSort/ - small arrays - Test error")
-	//	b.StopTimer()
-	//	for i := 0; i < b.N; i++ {
-	//		ar := generateSlice(10, 10)
-	//		b.StartTimer()
-	//		bubbleSort(ar)
-	//		b.StopTimer()
-	//	}
-	//})
+	b.ReportAllocs()
+	b.Run("small arrays", func(b *testing.B) {
+		//fmt.Println("BenchmarkMergeSort/ - small arrays - Test error")
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10, 10)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
 	//провал теста
-	//b.Run("middle arrays", func(b *testing.B) {
-	fmt.Println("BenchmarkMergeSort/ - middle arrays - Test error")
-	//	b.StopTimer()
-	//	for i := 0; i < b.N; i++ {
-	//		ar := generateSlice(100, 1000)
-	//		b.StartTimer()
-	//		bubbleSort(ar)
-	//		b.StopTimer()
-	//	}
-	//})
+	b.Run("middle arrays", func(b *testing.B) {
+		//fmt.Println("BenchmarkMergeSort/ - middle arrays - Test error")
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(100, 1000)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
 	b.Run("big arrays", func(b *testing.B) {
 		b.StopTimer()
 		for i := 0; i < b.N; i++ {
@@ -151,22 +220,47 @@ func BenchmarkMergeSort(b *testing.B) {
 			b.StopTimer()
 		}
 	})
-}
-
-//Задание 12.7.1
-//Реализуйте быструю сортировку.
-func BenchmarkQuickSort(b *testing.B) {
-	//провал теста
-	//b.Run("small arrays", func(b *testing.B) {
-	fmt.Println("BenchmarkQuickSort/ - small arrays - Test error")
+	//Добавить проверки для других размеров входных данных, отличающихся от примера, попробовать использовать
+	//большие и меньшие максимальные числа.
+	//малый размер данных
+	//b.Run("SMOLL_small arrays", func(b *testing.B) {
 	//	b.StopTimer()
 	//	for i := 0; i < b.N; i++ {
-	//		ar := generateSlice(10, 10)
+	//		ar := generateSlice(5, 10)
 	//		b.StartTimer()
 	//		bubbleSort(ar)
 	//		b.StopTimer()
 	//	}
 	//})
+	//большой размер данныйх
+	//провал теста
+	//b.Run("BIG_big arrays", func(b *testing.B) {
+	//	b.StopTimer()
+	//	for i := 0; i < b.N; i++ {
+	//		ar := generateSlice(100000, 400000)
+	//		b.StartTimer()
+	//		bubbleSort(ar)
+	//		b.StopTimer()
+	//	}
+	//})
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Задание 12.7.1
+//Реализуйте быструю сортировку.
+func BenchmarkQuickSort(b *testing.B) {
+	b.ReportAllocs()
+	//провал теста
+	b.Run("small arrays", func(b *testing.B) {
+		//fmt.Println("BenchmarkQuickSort/ - small arrays - Test error")
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10, 10)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
 
 	b.Run("middle arrays", func(b *testing.B) {
 		b.StopTimer()
@@ -187,9 +281,32 @@ func BenchmarkQuickSort(b *testing.B) {
 			b.StopTimer()
 		}
 	})
+	//Добавить проверки для других размеров входных данных, отличающихся от примера, попробовать использовать
+	//большие и меньшие максимальные числа.
+	//малый размер данных
+	//b.Run("SMOLL_small arrays", func(b *testing.B) {
+	//	b.StopTimer()
+	//	for i := 0; i < b.N; i++ {
+	//		ar := generateSlice(5, 10)
+	//		b.StartTimer()
+	//		bubbleSort(ar)
+	//		b.StopTimer()
+	//	}
+	//})
+	//большой размер данныйх
+	//провал теста
+	//b.Run("BIG_big arrays", func(b *testing.B) {
+	//	b.StopTimer()
+	//	for i := 0; i < b.N; i++ {
+	//		ar := generateSlice(100000, 400000)
+	//		b.StartTimer()
+	//		bubbleSort(ar)
+	//		b.StopTimer()
+	//	}
+	//})
 }
 
-//
+/////////////////////////////////////////////////////////////////////////////////////
 func generateSlice(max, size int) []int {
 	ar := make([]int, size)
 	for i := range ar {
