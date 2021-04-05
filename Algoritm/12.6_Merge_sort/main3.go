01a9a6a3c60f167bbe4405686a81ace99b216d66
Freepoint24 <5443006@gmail.com>
2021-04-05 22:01:32 +0300

    lesson 12.6_Merge_sort

---
+++ Algoritm/12.6_Merge_sort/main3.go
@@ -0,0 +1,57 @@
+package main
+
+func mergSort(arr []int []int)  {
+	item := make([]int, len(arr))
+	copy(items, arr)
+	doMergeSort(items)
+	return items
+	}
+func doMergSort(items []int)  {
+	lenght := len(items)
+	if lenght == 1 {
+		return
+	}
+
+	ILeft := lenght / 2
+	left := make([]int, ILeft)
+	copy(left, items[:ILeft])
+
+	IRight := lenght / 2
+	left := make([]int, IRight)
+	copy(left, items[:ILeft])
+
+	doMergSort(left)
+	doMergSort(right)
+
+	merge(left, right, items)
+
+}
+
+func merge(left []int, right []int, items []int)  {
+	l := 0
+	r := 0
+	i := 0
+
+	for (l < len(left)) && (r<len(right)){
+		if left[l] < right[r] {
+			result[i] = left[i]
+			l++
+		} else {
+			result[i] = right[r]
+				r++
+		}
+		i++
+	}
+	lenght := len(left) - l
+	copy(result[i:i + lenght], left[l:])
+	i = i + lenght
+	lenght = len(lenght, right[r:])
+
+}
+
+
+func main (){
+	items := []int{4, 1, 5, }
+	sortitem := mergSort(items)
+}
+
